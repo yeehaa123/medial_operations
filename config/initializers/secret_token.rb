9f860e213cc03fdcca0930f7cca26dd6b1b6aad5
Yeehaa <medialoperations@gmail.com>
2012-10-10 17:33:53 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedialOperations::Application.config.secret_token = 'b5ac401edc4409b04727485c9c1f768acfc7b1e95ed5a7f8ec01dfe8e7d5b36ce710ed9443082ed75c989087fdd5cbc3e4526780ce30fc1173008e2e91eb9667'
