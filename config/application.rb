8409399abd52ccaba05e49d11639b00ed1970247
Yeehaa <medialoperations@gmail.com>
2012-10-13 01:09:55 +0200

    fix compass problem

--- config/application.rb
+++ config/application.rb
@@ -61,8 +61,14 @@ module MedialOperations
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # YEEHAA: For Heroku
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # YEEHAA: Sass, Compass, and Susy
+    config.sass.preferred_syntax = :sass
 
     config.generators do |g|
         g.view_specs false
